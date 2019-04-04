4caab4a6cb3b5fd900dda846095da22d153d7bcb
Skavarotti <skavarotti@gmail.com>
2019-04-04 14:12:56 +0300

    Начало работы 04.04.2019

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Денис Скаваротти on 4/4/19.
+//  Copyright © 2019 Денис Скаваротти. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
