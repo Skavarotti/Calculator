9dbb186f7539148135be871c053ed05003cb8667
Skavarotti <skavarotti@gmail.com>
2019-04-10 01:25:26 +0300

    Доработка 10.04.2019

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     var firstValue: Double = 0//Значение до нажатия операнда
     var valueForPecent: Double = 0//Для %
     var mathSign: Bool = false//Метка был ли нажат операнд
-    var pointSign: Bool = false //Есть ли уже точка
+    var pointSignOne: Bool = false //Есть ли уже точка
     var valueOperation: String = ""//Переменаая в себе хранит нажатый знак для проверки после нажатия = и других действий (+-/*)
     
     
@@ -45,111 +45,165 @@ class ViewController: UIViewController {
     //Mark 2. Работа с операндами
     @IBAction func pressedOperand(_ sender: UIButton) {
         if resultLabel.text != "" && sender.currentTitle == "C" && sender.currentTitle == "=" && sender.currentTitle == "%" && sender.currentTitle == "+/-" && sender.currentTitle == "∙"{//Проверка на пустоту "", C, =
-        } else if sender.currentTitle == "+"{
+        } else if sender.currentTitle == "+" && resultLabel.text != ""{
             firstValue = Double(resultLabel.text!)!;
             labelSign.text = "+";
             valueOperation = labelSign.text!;//valueOperation = sender.currentTitle!
             valueForPecent = firstValue;//Для %
             mathSign = true;//При нажатии + mathSign true. Для ф-ии pressedNumbers
-            pointSign = false;//Для точки
-        } else if sender.currentTitle == "-"{
+            pointSignOne = false;//Для точки
+        } else if sender.currentTitle == "-" && resultLabel.text != ""{
             firstValue = Double(resultLabel.text!)!;
             labelSign.text = "-";
             valueOperation = labelSign.text!;
             valueForPecent = firstValue;
             mathSign = true;
-            pointSign = false;
-        } else if sender.currentTitle == "×"{
+            pointSignOne = false;
+        } else if sender.currentTitle == "×" && resultLabel.text != ""{
             firstValue = Double(resultLabel.text!)!;
             labelSign.text = "×";
             valueOperation = labelSign.text!;
             valueForPecent = firstValue;
             mathSign = true;
-            pointSign = false;
-        } else if sender.currentTitle == "÷"{
+            pointSignOne = false;
+        } else if sender.currentTitle == "÷" && resultLabel.text != ""{
             firstValue = Double(resultLabel.text!)!;
             labelSign.text = "÷";
             valueOperation = labelSign.text!;
             valueForPecent = firstValue;
             mathSign = true;
-            pointSign = false;
+            pointSignOne = false;
         }
     }
     
     
     //Mark 3. Работа с дополнительными операндами
     @IBAction func pressedSignChange(_ sender: UIButton) {
-        if mathSign != false{
-            let negativeValue = Double(resultLabel.text!)!
-            if resultLabel.text != "" {
-                resultLabel.text = String(negativeValue * negativeValue)
-                mathSign = true
+        var valueFreeNull = ""//Значение для удаления 0 после точки
+        if mathSign != false && resultLabel.text != ""{
+            let value: Double = Double(resultLabel.text!)!//Конвертер в Double
+            valueFreeNull = String(-value)//Конвертер из String в Double
+            var valueArray = valueFreeNull.components(separatedBy: ".")//Массив с разделитем ".". Число будет [0], а 0 будет [1]
+            if valueArray[1] == "0"{//Если равно 0 то вставится [0] число до точки [1]
+                resultLabel.text = "\(valueArray[0])"
+            } else {//Если нет 0 то встаится число без нуля
+                resultLabel.text = valueFreeNull
             }
+                mathSign = true
         }
     }
     
     @IBAction func pressedPoint(_ sender: UIButton) {
-        if pointSign != true{
-            let valueForPoint = Int(resultLabel.text!)!
+        if pointSignOne != true && resultLabel.text != ""{
+            let valueForPoint = String(resultLabel.text!)
             let point = "."
-            if resultLabel.text != "" {
-                resultLabel.text = String("\(valueForPoint)\(point)")
-                pointSign = true
-            }
+            resultLabel.text = String("\(valueForPoint)\(point)")
+            pointSignOne = true
         }
     }
     
     @IBAction func pressedPercent(_ sender: UIButton) {
+        var valueFreeNull = ""//Значение для удаления 0 после точки
         if resultLabel.text != "" {
             if valueOperation == "+"{
-                resultLabel.text = String(valueForPecent + (valueForPecent / 100 * valueFromScreen))
-                pointSign = false
+                valueFreeNull = String(valueForPecent + (valueForPecent / 100 * valueFromScreen))
+                var valueArray = valueFreeNull.components(separatedBy: ".")//Массив с разделитем ".". Число будет [0], а 0 будет [1]
+                if valueArray[1] == "0"{//Если равно 0 то вставится [0] число до точки [1]
+                    resultLabel.text = "\(valueArray[0])"
+                } else {//Если нет 0 то встаится число без нуля
+                    resultLabel.text = valueFreeNull
+                }
+                pointSignOne = false
             } else if valueOperation == "-"{
-                resultLabel.text = String(valueForPecent - (valueForPecent / 100 * valueFromScreen))
-                pointSign = false
+                valueFreeNull = String(valueForPecent - (valueForPecent / 100 * valueFromScreen))
+                var valueArray = valueFreeNull.components(separatedBy: ".")
+                if valueArray[1] == "0"{
+                    resultLabel.text = "\(valueArray[0])"
+                } else {
+                    resultLabel.text = valueFreeNull
+                }
+                pointSignOne = false
             } else if valueOperation == "×"{
-                resultLabel.text = String(valueForPecent * (valueForPecent / 100 * valueFromScreen))
-                pointSign = false
+                valueFreeNull = String(valueForPecent * (valueForPecent / 100 * valueFromScreen))
+                var valueArray = valueFreeNull.components(separatedBy: ".")
+                if valueArray[1] == "0"{
+                    resultLabel.text = "\(valueArray[0])"
+                } else {
+                    resultLabel.text = valueFreeNull
+                }
+                pointSignOne = false
             } else if valueOperation == "÷"{
-                resultLabel.text = String(valueForPecent / (valueForPecent / 100 * valueFromScreen))
-                pointSign = false
+                valueFreeNull = String(valueForPecent / (valueForPecent / 100 * valueFromScreen))
+                var valueArray = valueFreeNull.components(separatedBy: ".")
+                if valueArray[1] == "0"{
+                    resultLabel.text = "\(valueArray[0])"
+                } else {
+                    resultLabel.text = valueFreeNull
+                }
+                pointSignOne = false
             }
         }
         valueOperation = ""
     }
     
     @IBAction func pressedSum(_ sender: UIButton) {
+        var valueFreeNull = ""
         if valueOperation == "+"{
-            resultLabel.text = String(firstValue + valueFromScreen)
+            valueFreeNull = String(firstValue + valueFromScreen)
+            var valueArray = valueFreeNull.components(separatedBy: ".")
+            if valueArray[1] == "0"{
+                resultLabel.text = "\(valueArray[0])"
+            } else {
+                resultLabel.text = valueFreeNull
+            }
             valueForPecent = Double(resultLabel.text!)!
             mathSign = true
-            pointSign = false
+            pointSignOne = false
         } else if valueOperation == "-"{
-            resultLabel.text = String(firstValue - valueFromScreen)
+            valueFreeNull = String(firstValue - valueFromScreen)
+            var valueArray = valueFreeNull.components(separatedBy: ".")
+            if valueArray[1] == "0"{
+                resultLabel.text = "\(valueArray[0])"
+            } else {
+                resultLabel.text = valueFreeNull
+            }
             valueForPecent = Double(resultLabel.text!)!
             mathSign = true
-            pointSign = false
+            pointSignOne = false
         } else if valueOperation == "×"{
-            resultLabel.text = String(firstValue * valueFromScreen)
+            valueFreeNull = String(firstValue * valueFromScreen)
+            var valueArray = valueFreeNull.components(separatedBy: ".")
+            if valueArray[1] == "0"{
+                resultLabel.text = "\(valueArray[0])"
+            } else {
+                resultLabel.text = valueFreeNull
+            }
             valueForPecent = Double(resultLabel.text!)!
             mathSign = true
-            pointSign = false
+            pointSignOne = false
         } else if valueOperation == "÷"{
-            
-            resultLabel.text = String(firstValue / valueFromScreen)
+            valueFreeNull = String(firstValue / valueFromScreen)
+            var valueArray = valueFreeNull.components(separatedBy: ".")
+            if valueArray[1] == "0"{
+                resultLabel.text = "\(valueArray[0])"
+            } else {
+                resultLabel.text = valueFreeNull
+            }
             valueForPecent = Double(resultLabel.text!)!
             mathSign = true
-            pointSign = false
+            pointSignOne = false
         }
+        labelSign.text = ""
     }
     
     @IBAction func pressedButtonClear(_ sender: UIButton) {
         valueFromScreen = 0
         firstValue = 0
         mathSign = false
-        pointSign = false
+        pointSignOne = false
         valueOperation = ""
         resultLabel.text = ""
+        labelSign.text = ""
     }
 }
 
